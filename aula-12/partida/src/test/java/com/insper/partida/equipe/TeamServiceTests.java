772bf5af4b32b4fa647e1f40b534a3e404e1cf17
Eduardo Santana <ezambomsantana@gmail.com>
2023-03-21 11:52:38 -0300

    adiciona testes

--- aula-12/partida/src/test/java/com/insper/partida/equipe/TeamServiceTests.java
+++ aula-12/partida/src/test/java/com/insper/partida/equipe/TeamServiceTests.java
@@ -33,10 +33,7 @@ public class TeamServiceTests {
     @Test
     void test_listTeamsNotEmpty() {
 
-        Team team = new Team();
-        team.setId(1);
-        team.setIdentifier("time-1");
-        team.setName("Time 1");
+        Team team = getTeam();
 
         List<Team> lista = new ArrayList<>();
         lista.add(team);
@@ -49,13 +46,12 @@ public class TeamServiceTests {
         Assertions.assertEquals(1, resp.get(0).getId());
     }
 
+
     @Test
     void test_saveTeam() {
 
-        Team team = new Team();
-        team.setId(1);
-        team.setIdentifier("time-1");
-        team.setName("Time 1");
+
+        Team team = getTeam();
 
         Mockito.when(teamRepository.save(team)).thenReturn(team);
         Team resp = teamService.saveTeam(team);
@@ -65,5 +61,44 @@ public class TeamServiceTests {
         Assertions.assertEquals(team, resp);
 
     }
+
+    @Test
+    void test_getTeam() {
+
+        Team team = getTeam();
+
+        Mockito.when(teamRepository.findByIdentifier("time-1")).thenReturn(team);
+
+        Team resp = teamService.getTeam("time-1");
+
+        Assertions.assertEquals(1, resp.getId());
+
+    }
+
+    @Test
+    void test_deleteTeam() {
+
+        Team team = getTeam();
+
+        Mockito.when(teamRepository.findByIdentifier("time-1")).thenReturn(team);
+
+        Mockito.doNothing().when(teamRepository).delete(team);
+
+        teamService.deleteTeam("time-1");
+
+        Mockito.verify(teamRepository, Mockito.times(1))
+                .delete(team);
+    }
+
+
+
+    private static Team getTeam() {
+        Team team = new Team();
+        team.setId(1);
+        team.setIdentifier("time-1");
+        team.setName("Time 1");
+        return team;
+    }
+
 
 }
